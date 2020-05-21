a87ace170f21176a087bb2e560e93193a28af3fd
joshuacassidy <joshcassidy79@gmail.com>
2020-05-21 18:06:15 +0100

    initial commit

---
+++ ifs.pl
@@ -0,0 +1,34 @@
+
+$x = 10;
+
+if($x == 10) {
+    print("hello\n");
+}
+
+if($x == 9) {
+    print("hello\n");
+} elsif ($x == 10) {
+    print("hello 2\n");
+} 
+
+
+if($x == 9) {
+    print("hello\n");
+} elsif ($x == 8) {
+    print("hello 2\n");
+} else {
+    print("hello 3\n");
+}
+
+
+if (0 or 1) { 
+    print('true or', "\n")
+}
+
+if (not 0) {
+    print("not operator", "\n")
+}
+
+if (1 and 1) {
+    print("and operator", "\n")
+}
